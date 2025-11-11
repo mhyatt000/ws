cf080d41196ec8e0dd2fe8f4b8f2ef73b810f305
Matt Hyatt <mhyatt000@gmail.com>
2025-11-10 22:11:36 -0500

    wip

--- system.sh
+++ system.sh
@@ -1,1 +1,5 @@
 sudo apt install -y mesa-utils libgl1-mesa-dri libglx-mesa0 libglvnd0 mesa-vulkan-drivers 
+sudo apt install -y libspnav-dev spacenavd pkg-config
+sudo apt install -y libbluetooth-dev pkg-config
+sudo apt install -y libspnav-dev spacenavd pkg-config
+
