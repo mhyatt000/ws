fb23ff585c2718202ba67ab68a62a07620d1b072
Matthew Hyatt <mhyatt000@gmail.com>
2025-12-05 16:15:13 -0600

    rename env for clarity

---
+++ .pixi.sh
@@ -0,0 +1,5 @@
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export ROS_DOMAIN_ID=69
+
+[ -f "$PIXI_PROJECT_ROOT/install/setup.bash" ] && . "$PIXI_PROJECT_ROOT/install/setup.bash"
+
--- env.sh
+++
@@ -1,2 +0,0 @@
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-# export ROS_DOMAIN_ID=69
